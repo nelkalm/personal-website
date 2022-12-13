5aec5859cc480a020f0b7a27f2bc2197b4617be2
Nelson Lu <nelkalmlu@gmail.com>
2022-12-12 17:03:53 -0800

    project section implemented

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import ProjectItem from "./ProjectItem";
+
+import restlessCryptImg from "../public/assets/projects/restless-crypt.png";
+import hashmapImg from "../public/assets/projects/hashmap-sc.png";
+import reportSageImg from "../public/assets/projects/reportSage.png";
+import youTrainImg from "../public/assets/projects/youTrain.png";
 
 const Projects = () => {
-  return <div className="w-full">Projects</div>;
+  return (
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <h3 className="uppercase text-xl tracking-widest text-[#808000]">
+          Projects
+        </h3>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="ReportSage"
+            projectDescription="MongoDB, Express, React, Node, User Authentication"
+            backgroundImg={reportSageImg}
+            projectUrl="/reportsage"
+          />
+          <ProjectItem
+            title="HashMap Implementation"
+            projectDescription="Python, Data Structures, Hashing"
+            backgroundImg={hashmapImg}
+            projectUrl="/hashmap"
+          />
+          <ProjectItem
+            title="Restless Crypt"
+            projectDescription="Python, Pygame, Graphical User Interace, Object-Oriented Programming"
+            backgroundImg={restlessCryptImg}
+            projectUrl="/restless-crypt"
+          />
+          <ProjectItem
+            title="YouTrain"
+            projectDescription="MongoDB, Express, React, Node"
+            backgroundImg={youTrainImg}
+            projectUrl="/youTrain"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
